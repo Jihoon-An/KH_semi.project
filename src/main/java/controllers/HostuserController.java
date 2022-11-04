fc9327491fc6e29eed0d4a7abc1a4384922b674e
yookyungmin <ykmykm4608@naver.com>
2022-11-04 17:42:26 +0900

    hostusercontroller add

---
+++ src/main/java/controllers/HostuserController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("*.host")
+public class HostuserController extends ControllerAbs {
+	
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
