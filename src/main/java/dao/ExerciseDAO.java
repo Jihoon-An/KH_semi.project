0ef2b158ba4190671990a28a199869415c1f4580
dOcOb <abg1418@gmail.com>
2022-11-06 17:39:21 +0900

    create deleteByUserSeq() in ExerciseDAO

--- src/main/java/dao/ExerciseDAO.java
+++ src/main/java/dao/ExerciseDAO.java
@@ -1,4 +1,27 @@
 package dao;
 
-public class ExerciseDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ExerciseDAO extends Dao{
+
+    protected ExerciseDAO() {
+        super();
+    }
+
+    public static ExerciseDAO getInstance(){
+        return (ExerciseDAO) Dao.getInstance();
+    }
+
+    public void deleteByUserSeq(int userSeq) throws Exception{
+        String sql = "delete from exercise where user_seq = ?";
+        try(Connection connection = this.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+        ){
+            statement.setInt(1, userSeq);
+            statement.executeUpdate();
+
+            connection.commit();
+        }
+    }
 }
