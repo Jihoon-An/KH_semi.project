f78907c9aa2149b60762aeb780955648e7bb773c
dOcOb <abg1418@gmail.com>
2022-11-06 16:22:49 +0900

    save data

---
+++ src/main/java/dto/MembershipDTO.java
@@ -0,0 +1,14 @@
+package dto;
+
+import java.sql.Timestamp;
+
+public class MembershipDTO {
+    private int mem_seq;
+    private int gym_seq;
+    private int bs_seq;
+    private int user_seq;
+    private Timestamp mem_date;
+    private Timestamp mem_end;
+    private String mem_note;
+
+}
