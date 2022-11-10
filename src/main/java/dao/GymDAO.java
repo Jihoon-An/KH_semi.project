7c7df69526f071826da14e862fc12ebbb997bb41
MinS2op <minseop9908@naver.com>
2022-11-10 12:12:38 +0900

    1110 12:12 save

--- src/main/java/dao/GymDAO.java
+++ src/main/java/dao/GymDAO.java
@@ -8,10 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import dto.FavoritesDTO;
-import dto.GymDTO;
-import dto.ReviewDTO;
-import dto.UserDTO;
+import dto.*;
 
 import javax.xml.transform.Result;
 
@@ -217,7 +214,12 @@ public class GymDAO extends Dao {
     }
 
 
-
+    /**
+     * Gym 시설 정보 수정
+     *
+     * @param gymDto
+     * @throws Exception
+     */
     public void modifyGym(GymDTO gymDto) throws Exception {
         String sql = "update gym set "
                    + "gym_name = ?, "
@@ -228,13 +230,45 @@ public class GymDAO extends Dao {
                    + "gym_location = ?, "
                    + "gym_x = ?, "
                    + "gym_y = ?, "
-                   + "gym_main_sysImg = ?"
+                   + "gym_main_sysImg = ? "
                    + "where gym_seq = ?";
 
         try (Connection con = this.getConnection();
                 PreparedStatement pstat = con.prepareStatement(sql);) {
+            pstat.setString(1, gymDto.getGym_name());
+            pstat.setString(2, gymDto.getGym_phone());
+            pstat.setString(3, gymDto.getGym_price());
+            pstat.setString(4, gymDto.getGym_open());
+            pstat.setString(5, gymDto.getGym_close());
+            pstat.setString(6, gymDto.getGym_x());
+            pstat.setString(7, gymDto.getGym_y());
+            pstat.setString(8, gymDto.getGym_main_sysImg());
+            pstat.setInt(9, gymDto.getGym_seq());
 
+            pstat.executeUpdate();
+            con.commit();
+        }
+    }
 
+    /**
+     * Gym Filter 시설 필터 수정
+     *
+     * @param gymFilterDTO
+     * @throws Exception
+     */
+    public void modifyGymFilter(GymFilterDTO gymFilterDTO) throws Exception {
+        String sql = "update gym_filter set open = ?, locker = ?, shower = ?, park =? where gym_seq = ?";
+
+        try (Connection con = this.getConnection();
+                PreparedStatement pstat = con.prepareStatement(sql);) {
+            pstat.setString(1, gymFilterDTO.getOpen());
+            pstat.setString(2, gymFilterDTO.getLocker());
+            pstat.setString(3, gymFilterDTO.getShower());
+            pstat.setString(4, gymFilterDTO.getPark());
+            pstat.setInt(5, gymFilterDTO.getGym_seq());
+
+            pstat.executeUpdate();
+            con.commit();
         }
     }
 
@@ -244,4 +278,6 @@ public class GymDAO extends Dao {
 
 
 }
+
+
 
