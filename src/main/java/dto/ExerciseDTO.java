8e1623ceb2b8099645e45a7f8078009a83cf87a3
jaeyoung <jaeyoung1870@gmail.com>
2022-11-10 15:32:16 +0900

    modify ExerciseDTO

--- src/main/java/dto/ExerciseDTO.java
+++ src/main/java/dto/ExerciseDTO.java
@@ -4,90 +4,116 @@ import java.sql.ResultSet;
 import java.sql.Timestamp;
 
 public class ExerciseDTO {
-    private int exr_seq;
-    private int cal_seq;
-    private Timestamp cal_date;
-    private int user_seq;
-    private String exr_sports;
-    private String exr_how;
-    private String exr_intensity;
+	private int exr_seq;
+	private int cal_seq;
+	private Timestamp cal_date;
+	private int user_seq;
+	private String exr_memo;
+	private String exr_how;
+	private String exr_intensity;
+	private String inbody_weight;
+	private String inbody_bfm;
+	private String inbody_bmi;
+	private String inbody_sm;
 
-    public ExerciseDTO() {
-    }
 
-    public ExerciseDTO(ResultSet resultSet) throws Exception{
-        this.exr_seq = resultSet.getInt("exr_seq");
-        this.cal_seq = resultSet.getInt("cal_seq");
-        this.cal_date = resultSet.getTimestamp("cal_date");
-        this.user_seq = resultSet.getInt("user_seq");
-        this.exr_sports = resultSet.getString("exr_sports");
-        this.exr_how = resultSet.getString("exr_how");
-        this.exr_intensity = resultSet.getString("exr_intensity");
-    }
+	public ExerciseDTO() {}
+	
+	public ExerciseDTO(ResultSet resultSet) throws Exception{
+		this.exr_seq = resultSet.getInt("exr_seq");
+		this.cal_seq = resultSet.getInt("cal_seq");
+		this.cal_date = resultSet.getTimestamp("cal_date");
+		this.user_seq = resultSet.getInt("user_seq");
+		this.exr_memo = resultSet.getString("exr_memo");
+		this.exr_how = resultSet.getString("exr_how");
+		this.exr_intensity = resultSet.getString("exr_intensity");
+		this.inbody_weight = resultSet.getString("inbody_weight");
+		this.inbody_bfm = resultSet.getString("inbody_bfm");
+		this.inbody_bmi = resultSet.getString("inbody_bmi");
+		this.inbody_sm = resultSet.getString("inbody_sm");
+	}	
 
-    public ExerciseDTO(int exr_seq, int cal_seq, Timestamp cal_date, int user_seq, String exr_sports, String exr_how, String exr_intensity) {
-        this.exr_seq = exr_seq;
-        this.cal_seq = cal_seq;
-        this.cal_date = cal_date;
-        this.user_seq = user_seq;
-        this.exr_sports = exr_sports;
-        this.exr_how = exr_how;
-        this.exr_intensity = exr_intensity;
-    }
+	public ExerciseDTO(int exr_seq, int cal_seq, Timestamp cal_date, int user_seq, String exr_sports, String exr_how,
+			String exr_intensity, String inbody_weight, String inbody_bfm, String inbody_bmi, String inbody_sm) {
+		super();
+		this.exr_seq = exr_seq;
+		this.cal_seq = cal_seq;
+		this.cal_date = cal_date;
+		this.user_seq = user_seq;
+		this.exr_memo = exr_sports;
+		this.exr_how = exr_how;
+		this.exr_intensity = exr_intensity;
+		this.inbody_weight = inbody_weight;
+		this.inbody_bfm = inbody_bfm;
+		this.inbody_bmi = inbody_bmi;
+		this.inbody_sm = inbody_sm;
+	}
+	public int getExr_seq() {
+		return exr_seq;
+	}
+	public void setExr_seq(int exr_seq) {
+		this.exr_seq = exr_seq;
+	}
+	public int getCal_seq() {
+		return cal_seq;
+	}
+	public void setCal_seq(int cal_seq) {
+		this.cal_seq = cal_seq;
+	}
+	public Timestamp getCal_date() {
+		return cal_date;
+	}
+	public void setCal_date(Timestamp cal_date) {
+		this.cal_date = cal_date;
+	}
+	public int getUser_seq() {
+		return user_seq;
+	}
+	public void setUser_seq(int user_seq) {
+		this.user_seq = user_seq;
+	}
+	public String getExr_sports() {
+		return exr_memo;
+	}
+	public void setExr_sports(String exr_sports) {
+		this.exr_memo = exr_sports;
+	}
+	public String getExr_how() {
+		return exr_how;
+	}
+	public void setExr_how(String exr_how) {
+		this.exr_how = exr_how;
+	}
+	public String getExr_intensity() {
+		return exr_intensity;
+	}
+	public void setExr_intensity(String exr_intensity) {
+		this.exr_intensity = exr_intensity;
+	}
+	public String getInbody_weight() {
+		return inbody_weight;
+	}
+	public void setInbody_weight(String inbody_weight) {
+		this.inbody_weight = inbody_weight;
+	}
+	public String getInbody_bfm() {
+		return inbody_bfm;
+	}
+	public void setInbody_bfm(String inbody_bfm) {
+		this.inbody_bfm = inbody_bfm;
+	}
+	public String getInbody_bmi() {
+		return inbody_bmi;
+	}
+	public void setInbody_bmi(String inbody_bmi) {
+		this.inbody_bmi = inbody_bmi;
+	}
+	public String getInbody_sm() {
+		return inbody_sm;
+	}
+	public void setInbody_sm(String inbody_sm) {
+		this.inbody_sm = inbody_sm;
+	}
 
-    public int getExr_seq() {
-        return exr_seq;
-    }
 
-    public void setExr_seq(int exr_seq) {
-        this.exr_seq = exr_seq;
-    }
-
-    public int getCal_seq() {
-        return cal_seq;
-    }
-
-    public void setCal_seq(int cal_seq) {
-        this.cal_seq = cal_seq;
-    }
-
-    public Timestamp getCal_date() {
-        return cal_date;
-    }
-
-    public void setCal_date(Timestamp cal_date) {
-        this.cal_date = cal_date;
-    }
-
-    public int getUser_seq() {
-        return user_seq;
-    }
-
-    public void setUser_seq(int user_seq) {
-        this.user_seq = user_seq;
-    }
-
-    public String getExr_sports() {
-        return exr_sports;
-    }
-
-    public void setExr_sports(String exr_sports) {
-        this.exr_sports = exr_sports;
-    }
-
-    public String getExr_how() {
-        return exr_how;
-    }
-
-    public void setExr_how(String exr_how) {
-        this.exr_how = exr_how;
-    }
-
-    public String getExr_intensity() {
-        return exr_intensity;
-    }
-
-    public void setExr_intensity(String exr_intensity) {
-        this.exr_intensity = exr_intensity;
-    }
 }
